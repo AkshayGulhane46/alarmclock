a67d5d7a161e586120f705423f6edadda30537a9
Akshay Gulhane <argulhane10246@gmail.com>
2023-02-23 12:42:17 +0530

    Commit2302231

--- index.js
+++ index.js
@@ -4,108 +4,94 @@ let seconds;
 let timeofday;
 
 var inputminutes = 0;
-var inputhours  = 0;
-var inputseconds= 0;
+var inputhours = 0;
+var inputseconds = 0;
 
-var alarms = [];
-var alarm;
+var alarms = []; // storing alarms in array 
+var alarm = 0;
 
-
-
-window.onload = function(){
-    const timeID = setInterval(timeAndDate, alarmRing  ,1000);
+window.onload = function() {
+    const timeID = setInterval(timeAndDate, 1000); // every second time and Date will be called
     function timeAndDate() {
         const Masterdate = new Date();
         hour = Masterdate.getHours();
         minutes = Masterdate.getMinutes();
-        seconds = Masterdate.getSeconds ();
+        seconds = Masterdate.getSeconds();
+        // To show time in 12 hour format 
         timeofday = hour >= 12 ? 'PM' : 'AM'; // if greater then 12 then PM or else AM
         hour = hour % 12; // find current AM PM
         hour = hour ? hour : 12; // show 00 as 12 
         minutes = minutes < 10 ? '0' + minutes : minutes;
-
-        document.getElementById("Hour").innerHTML = hour;
-        document.getElementById("Minute").innerHTML = minutes;
-        document.getElementById("Second").innerHTML = seconds;
-        document.getElementById("timeofday").innerHTML = timeofday;
-        alarmRing();
-        alarm = hour + ":"+ minutes + ":"+ seconds +":" + timeofday;
-
-    }
-
-    function alarmRing(){
-        if(inputhours == hour && inputminutes == minutes && inputseconds == seconds){
-            window.alert("Alarm Ring")
+        // Set the time in respecitve divs 
+        const currentTime = hour + ":" + minutes + ":" + seconds + ":" + timeofday
+        document.getElementById("TIME").innerHTML = currentTime;
+        if (alarms.includes(currentTime)) { // Check if current time mateches with any of the alarm set 
+            alarmRing(); // if yes then call the alarm ring function 
         }
     }
 
+    function alarmRing() {
+        window.alert("Alarm Ring")
+    }
 };
 
-function setAlarm(){
-
-  
-
-    let list = document.getElementById("myList");
+function setAlarm() {
+    let list = document.getElementById("alarms-list-ul");
     inputminutes = document.getElementById("Minute-input").value;
-    inputhours =  document.getElementById("Hour-input").value;
+    inputhours = document.getElementById("Hour-input").value;
     inputseconds = document.getElementById("Second-input").value;
     inputtimeofday = document.getElementById("timeofday-input").value;
 
-    if(inputminutes > 60 || inputminutes < 0 || 
-        inputhours < 0|| inputhours > 12 || 
+    // input forms validations 
+
+    if (inputminutes > 60 || inputminutes < 0 ||
+        inputhours < 0 || inputhours > 12 ||
         inputseconds < 0 || inputseconds > 60
-        ){
-         window.alert("add Correct time please")
-         return
+    ) {
+        window.alert("Enter Correct time please")
+        return
     }
 
+    // elemtnt to be created for alarm inputs
+    const alarm = inputhours + ":" + inputminutes + ":" + inputseconds + ":" + inputtimeofday;
 
+    if (!alarms.includes(alarm)) { // Check if the same alarm is present or not 
+        alarms.push(alarm); // if not then push the alarm in array 
+        let li = document.createElement("li"); // Create a List Item in the alarm list
+        li.className = alarm;
+        li.id = "alarm"; // Set ID for list item for styling 
+        var deleteBtn = document.createElement("button");
+        deleteBtn.className = "Delete"; // Set ClasssName for delete button for styling 
 
+        li.innerText = alarm; // set inner text same as alarm time 
+        list.appendChild(li); // add the list item in our unordered list
+        li.appendChild(deleteBtn); // add the delete button on list item 
 
-    const alarm = inputhours + ":"+ inputminutes + ":"+ inputseconds +":"+inputtimeofday;
-    if(!alarms.includes(alarm)){
-         alarms.push(alarm );
-         let li = document.createElement("li");
-         li.className = alarm;
-         var deleteBtn = document.createElement("button");
-         deleteBtn.innerText = "delete";
-         deleteBtn.className = "delete";
-         li.innerText = alarm;
-        list.appendChild(li);
-        li.appendChild(deleteBtn);
+        var TrashIcon = document.createElement("i"); // trash can icon from font awsome 
+        TrashIcon.className = "fa-solid fa-trash";
+        deleteBtn.appendChild(TrashIcon); // add the icon to the delete button as child 
 
-    }else{
-        console.log("alrady in alarms")
+    } else {
+        window.alert("You already have same alarm set for " + alarm); // if user tries to add same alarm then show alert
+
     }
 
-    var closebtns = document.getElementsByClassName("delete");
+    var deletebtns = document.getElementsByClassName("Delete"); // count the number of delete buttons on page
     var i;
-    for (i = 0; i < closebtns.length; i++) {
-    closebtns[i].addEventListener("click", function() {
-    const value = this.parentElement.className;
-    console.log("Delete is prssed for" + value);
+    for (i = 0; i < deletebtns.length; i++) {
+        deletebtns[i].addEventListener("click", function() {
+            const value = this.parentElement.className; // value is taken from each delete button className 
+            //console.log("Delete is Pressed for " + value); 
 
-    if(alarms.includes(value)){
-        const index = alarms.indexOf(value);
-        alarms.splice(index, 1);
-         inputminutes = 0;
-         inputhours  = 0;
-         inputseconds= 0;
-     }
-
-    this.parentElement.style.display = 'none';
-  });
-}
-}
-
-function showList(){
-    for(var i = 0 ; i < alarms.length ; i++){
-        console.log(alarms[i]);
+            if (alarms.includes(value)) { // if array contains the alarm 
+                const index = alarms.indexOf(value);
+                alarms.splice(index, 1); // delte the alarm from alarms array 
+            }
+            this.parentElement.style.display = 'none'; // remove element form DOM 
+        });
     }
 }
 
-
-
-
-
-
+for (var i = 0; i < alarms.length; i++) {
+    console.log(alarms[i]);
+}
